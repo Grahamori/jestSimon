eac6cba83cb1acd363727c414c72d9f82fddadfd
Grahamori <grahamori@yahoo.co.uk>
2022-09-02 11:21:41 +0000

    advanced tests complete

--- scripts/game.js
+++ scripts/game.js
@@ -10,12 +10,26 @@ function newGame() {
     game.currentGame = [];
     game.playerMoves = [];
     showScore();
+    addTurn();
 };
 
 function showScore() {
     document.getElementById("score").innerText = game.score;
 };
 
+function addTurn() {
+    game.playerMoves = [];
+    game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]);
+    // showTurns();
+};
+
+function lightsOn(circ) {
+    document.getElementById(circ).classList.add(circ + "light");
+    setTimeout(function () {
+        document.getElementById(circ).classList.remove(circ + "light");
+    }, 400);
+};
 
 
-module.exports = { game, newGame, showScore };
+
+module.exports = { game, newGame, showScore, addTurn, lightsOn };
